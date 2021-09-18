fb5ab7ca124a9734bfcf108e4bcc746553b98049
Ubuntu <ubuntu@ip-172-26-14-246.ap-northeast-2.compute.internal>
2021-09-18 12:28:22 +0000

    1. add reviews & imgs  2. impl review log proto

--- blooferblog.py
+++ blooferblog.py
@@ -1,14 +1,34 @@
 from flask import Flask, render_template
 import reviewparser
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    rString = reviewparser.getReview(1)
-    rTitle = reviewparser.getReviewTitle(rString)
-    aTitle = reviewparser.getArticleTitle(rString, 1)
-    rContent = reviewparser.getReviewContent(rString)
-    return render_template('index.html', atitle=aTitle, title=rTitle, content=rContent)
+    path, dirs, files = next(os.walk("/home/ubuntu/BlooferBlog/static/reviews"))
+    file_count = len(files)
+
+    rStringList = []
+    rTitleList = []
+    aTitleList = []
+    rContentList = []
+    for i in range(1, file_count+1):
+        rStringList.append(reviewparser.getReview(i))
+        rTitleList.append(reviewparser.getReviewTitle(rStringList[i-1]))
+        aTitleList.append(reviewparser.getArticleTitle(rStringList[i-1], i))
+        rContentList.append(reviewparser.getReviewContent(rStringList[i-1]))
+
+
+    rTitle = reviewparser.getReviewTitle(rStringList[0])
+    aTitle = reviewparser.getArticleTitle(rStringList[0], 1)
+    rContent = reviewparser.getReviewContent(rStringList[0])
+
+
+    return render_template('index.html', atitleList=aTitleList, titleList=rTitleList, contentsList=rContentList, iter=file_count)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
--- reviewparser.py
+++ reviewparser.py
@@ -2,7 +2,7 @@ import os
 import blooferblog
 
 
-num2kor = {1:'하나', 2:'둘', 3:'셋', 4:'넷', 5:'다섯', 6:'여섯', 7:'일곱', 8:'여덟', 9:'아홉',
+num2kor = {0:'', 1:'하나', 2:'둘', 3:'셋', 4:'넷', 5:'다섯', 6:'여섯', 7:'일곱', 8:'여덟', 9:'아홉',
            10:'열', 20:'스물', 30:'서른', 40:'마흔', 50:'쉰', 60:'예순', 70:'일흔', 80:'여든', 90:'아흔',
            100:'백'}
 
@@ -17,10 +17,10 @@ def getReview(rNum):
 def getReviewContent(rString):
     lines = rString.splitlines()
     cnt = 0
-    rContent = ''
+    rContent = []
     for line in lines:
         if cnt > 0:
-            rContent += line + '\r\n'
+            rContent.append(line)
         cnt += 1
 
     return rContent
@@ -35,18 +35,26 @@ def getReviewTitle(rString):
 def getNumToKorean(num):
     if num < 10:
         return num2kor[num]
-    elif num < 100:
-        return num2kor[num/10]+num2kor[num%10]
+    elif num >= 10 and num < 100:
+        q, r = divmod(num, 10)
+        return num2kor[q*10] + num2kor[r]
     else:
-        return num2kor[num/100]+num2kor[(num%100)/10]+num2kor[num%10]
+        return str(num2kor[num/100])+str(num2kor[(num%100)/10])+str(num2kor[num%10])
 
 
 def getArticleTitle(rString, rNum):
     rTitle = rString.splitlines()
-    rNumKor = '하나'
     bName = rTitle[0].split('-')
 
     return '책 리뷰 '+getNumToKorean(rNum)+'. '+bName[0]
 
-def getReviewCount():
-    return 0
+class Review:
+
+    def __init__(self):
+        self.rNum = 0           # 리뷰 번호 (e.g. 책 리뷰 하나.)
+        self.rating = 0         # 리뷰 별점 (별점 5개 기준 1~10)
+        self.reviewTitle = ''   # 리뷰 제목 (e.g. 직업으로서의 소설가 - 무라카미 하루키 ★★★★)
+        self.reviewContent = '' # 리뷰 내용
+        self.articleTitle = ''  # 글 타이틀 (e.g. 책 리뷰 마흔다섯. 직업으로서의 소설가)
+
+        
